25fc5ff1f1d8f7ff721f5da309a2937a2ffc3b8a
Minh Trần <tanminh080803@gmail.com>
2026-01-23 22:09:23 +0700

    -rm dist

--- dist/routes/products.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const products_1 = require("../controllers/products");
-const authMiddleware_1 = require("../middlewares/authMiddleware");
-const productsRouter = (0, express_1.Router)();
-productsRouter.get("/products-list", authMiddleware_1.authMiddleware, products_1.getProducts);
-productsRouter.post("/products", authMiddleware_1.authMiddleware, products_1.createProduct);
-exports.default = productsRouter;
